6e3aa7515d9478e6862f428a24424a8df00c9f09
Vas Gábor <gvas.munka@gmail.com>
2018-01-06 17:07:50 +0100

    PropTypes are removed from the production (minified) builds.

--- rollup.config.js
+++ rollup.config.js
@@ -1,52 +1,44 @@
 import babel from 'rollup-plugin-babel'
 import uglify from 'rollup-plugin-uglify'
+import { minify } from 'uglify-es'
 import pkg from './package.json'
 import replaceExt from 'replace-ext'
 
 const config = {
   input: 'src/AutolinkerWrapper.js',
   external: ['autolinker', 'react', 'prop-types'],
-  plugins: [babel({ exclude: ['node_modules/**'] })],
+  plugins: process.env.NODE_ENV == 'production'
+    ? [
+      babel({ exclude: ['node_modules/**'] }),
+      uglify({}, minify),
+    ]
+    : [
+      babel({ exclude: ['node_modules/**'] }),
+    ],
 }
 
+const getName = name =>
+  process.env.NODE_ENV == 'production'
+    ? replaceExt(name, '.min.js')
+    : name
+
 export default [
-  // browser-friendly UMD build
-  {
-    ...config,
-    output: {
-      file: pkg.browser,
-      format: 'umd',
-      name: 'AutolinkerWrapper',
-      globals: {
-        autolinker: 'Autolinker',
-        react: 'React',
-        'prop-types': 'PropTypes',
-      },
-    },
-  },
-
-  // minified browser-friendly UMD build
-  {
-    ...config,
-    output: {
-      file: replaceExt(pkg.browser, '.min.js'),
-      format: 'umd',
-      name: 'AutolinkerWrapper',
-      globals: {
-        autolinker: 'Autolinker',
-        react: 'React',
-        'prop-types': 'PropTypes',
-      },
-    },
-    plugins: [...config.plugins, uglify()],
-  },
-
-  // CommonJS (for Node) and ES module (for bundlers) build.
   {
     ...config,
     output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' },
+      {
+        // browser-friendly UMD build
+        file: getName(pkg.browser),
+        format: 'umd',
+        name: 'AutolinkerWrapper',
+        globals: {
+          autolinker: 'Autolinker',
+          react: 'React',
+          'prop-types': 'PropTypes',
+        },
+      },
+      { /*CommonJS (for Node)*/ file: getName(pkg.main), format: 'cjs' },
+      { /*ES module (for bundlers)*/ file: getName(pkg.module), format: 'es' },
     ],
   },
 ]
